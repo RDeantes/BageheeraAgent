58558e1b118a7da1fbbc50ff261e0f2f1bb0b043
RDeantes <ludeantes@gmail.com>
2026-05-04 14:17:22 -0600

    AUT

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from BagheeraBrain import generar_contrato_automatico
+from BagheeraBrain import generar_contrato_directo
 
 app = FastAPI()
 
 @app.post("/contrato")
 def generar(data: dict):
 
-    archivo = generar_contrato_automatico(
+    archivo = generar_contrato_directo(
         data["nombre"],
         data["fecha_inicio"],
         data["fecha_termino"]

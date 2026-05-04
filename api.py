0fda12429e8cbfe77a2a32c0cb2c7d2a0e5070a2
Reyna Deantes <158773042+RDeantes@users.noreply.github.com>
2026-05-04 14:23:20 -0600

    Update API to use generar_contrato_automatico

--- api.py
+++ api.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
-from BagheeraBrain import generar_contrato_directo
+from BagheeraBrain import generar_contrato_automatico
 
 app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "Bagheera API activa 🐱"}
 
 @app.post("/contrato")
 def generar(data: dict):
 
-    archivo = generar_contrato_directo(
+    archivo = generar_contrato_automatico(
         data["nombre"],
         data["fecha_inicio"],
         data["fecha_termino"]
